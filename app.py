45e770a9a39338f60ef53d7e5f05874ea35d7a2c
jaikrish.dxj@gmail.com <Sjaikrish.Jaikrish@pb.com>
2020-07-21 12:19:43 +0530

    first page form is working

--- app.py
+++ app.py
@@ -6,25 +6,37 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-@app.route('/math', methods=['POST'])  # This will be called from UI
+@app.route('/math', methods=['GET','POST'])  # This will be called from UI
 def math_operation():
-    if (request.method=='POST'):
-        operation=request.form['operation']
-        num1=int(request.form['num1'])
-        num2 = int(request.form['num2'])
-        if(operation=='add'):
-            r=num1+num2
-            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
-        if (operation == 'subtract'):
-            r = num1 - num2
-            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        if (operation == 'multiply'):
-            r = num1 * num2
-            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        if (operation == 'divide'):
-            r = num1 / num2
-            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
-        return render_template('results.html',result=result)
+    if request.method == 'POST':
+
+        # age = request.form['age']
+
+        image = request.form['image']
+        number = request.form['number']
+        hell = image+number
+        # operation=request.form['operation']
+        # num1=int(request.form['num1'])
+        # num2 = int(request.form['num2'])
+        # print(num1)
+        # print(num2)
+        # if(operation=='add'):
+        #     r=num1+num2
+        #     result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        # if (operation == 'subtract'):
+        #     r = num1 - num2
+        #     result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        # if (operation == 'multiply'):
+        #     r = num1 * num2
+        #     result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        # if (operation == 'divide'):
+        #     r = num1 / num2
+        #     result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+
+
+        return render_template('results.html',result=hell)
+
+    return render_template('index.html')    
 
 if __name__ == '__main__':
     app.run(debug=True)        
