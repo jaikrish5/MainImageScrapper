d02373d49dd6eb25d2dc099046cccc844f5fbec3
jaikrish.dxj@gmail.com <Sjaikrish.Jaikrish@pb.com>
2020-07-22 12:10:55 +0530

    photos are geetting stored local with respc. queries

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from urllib.request import urlopen as uReq
 
 app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+print(APP_ROOT)
+
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
 def home_page():
     return render_template('index.html')
@@ -16,33 +19,36 @@ def math_operation():
     if request.method == 'POST':
 
         query =str(request.form['image'])
-        #n_images = int(request.form['number'])
 
-        site = "https://www.shutterstock.com/search/"
+        target = os.path.join(APP_ROOT,'static\\photos\\')
+        target_folder = os.path.join(target, '_'.join(query.lower().split(' ')))
+        print(target_folder)
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+            site = "https://www.shutterstock.com/search/"
+            searchquery = site+query
+            print(searchquery)
+            client = uReq(searchquery)
+            soup3 = BeautifulSoup(client.read(), 'html.parser')
+            links = []
+            for link in soup3.find_all("img", {"class": "z_h_c z_h_e"}):
+                imageLink = link['src']
+                links.append(imageLink)
 
-        
-        searchquery = site+query
-        print(searchquery)
+            # image_data = []
+            # for index,img_link in enumerate(links):
+            #     img_data = rq.get(img_link).content
+            #     image_data.append(img_data)  
 
-        client = uReq(searchquery)
-
-        #r2 = rq.get(searchquery).text
-
-        soup3 = BeautifulSoup(client.read(), 'html.parser')
-
-        links = []
-        for link in soup3.find_all("img", {"class": "z_h_c z_h_e"}):
-            imageLink = link['src']
-            links.append(imageLink)
+            for index,img_link in enumerate(links):
+                img_data = rq.get(img_link).content
+                with open(target_folder+"/"+str(index+1)+'.jpg','wb+') as f:
+                    f.write(img_data)
 
 
-
-        
-        
-
-
-        return render_template('results.html',result=links)
-
+            return render_template('results.html',result=links)
+        else:
+            pass
     return render_template('index.html')    
 
 if __name__ == '__main__':
